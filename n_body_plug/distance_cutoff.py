5e95ba4bc88842f57f180c598615441039a2d12c
Ben Peyton <bgpeyton@vt.edu>
2018-08-29 17:19:15 -0400

    Cutoff and cooking working, now need to migrate distance setting and close_solvent selection to this script

--- n_body_plug/distance_cutoff.py
+++ n_body_plug/distance_cutoff.py
@@ -6,53 +6,121 @@ def main():
     '''
     Main function
     '''
-    method = 'cam-b3lyp'
-    kwargs = ['cutoff_rotation', 'cutoff_solute_rotation']
+    # Define method, result(s) to add, grab database 
+    method = 'b3lyp'
+    results = ['cutoff_rotation', 'cutoff_solute_rotation']
     db = shelve.open(db_name, writeback=True)
+    n_max = db[method]['n_body_max']
     
+    # Make sure database has been filled out by n_body driver
     if sanity_check(db):
         print("Sanity check passed!")
+    else: # Should raise Exception for issue
+        print("Sanity check not passed!")
 
-    extend(db, kwargs)
+    # NOTE: Need to generate close_solvent here I think, rather than at db generation
+    for result in results:
+        # Extend the database to hold the new result(s)
+        db = extend(db, method, result)
 
-    n_max = db[method]['n_body_max']
-    for result in kwarg:
+        # Cut the irrelevant data for result(s) out of 'raw_data' and store
         for n in range(1,n_max+1):
-            db[method][n][kwarg]['raw_data'] = cutoff(db, method, n, 'rotation')
+            db[method][n][result]['raw_data'] = cutoff(db, method, n, 
+                                                       result.replace('cutoff_',''))
 
-    # Need to generate close_solvent here I think, rather than at db generation
-    # Final step: Cook cutoff data
+            # Cook new result(s) data
+            cook(db, method, n, result)
 
 
-def cutoff(db, method, n, full_result, dist = 0)
+def cook(db, method, n, result):
+    '''
+    Cook the data as in the n_body driver, one result at a time
+    '''
+    # Automatic cooking requires all result data be stored as lists in the
+    # database. Even if the result is a single number (i.e. energies)
+    cooked_data = collections.OrderedDict()
+    raw_data    = collections.OrderedDict()
+
+    # Read in all cooked_data for m < n
+    for m in range(1, n):
+        cooked_data[m] = copy.deepcopy(db[method][m][result]['cooked_data'])
+
+    # Read in raw data for m
+    cooked_data[n] = copy.deepcopy(db[method][n][result]['raw_data'])
+    if len(cooked_data[n]) == 0: # No raw_data, cutoff probably cut all n-body jobs
+        db[method][n][result][result] = copy.deepcopy(db[method][n-1][result][result])
+        print('No raw_data for {}-body {}, distance_cutoff has likely been reached'.format(n,result))
+        return
+
+    # Cook n data
+    for m in range(1,n):
+        for n_key in cooked_data[n]:
+            for m_key in cooked_data[m]:
+                m_set = set(m_key.split('_'))
+                n_set  = set(n_key.split('_'))
+                if len(m_set.intersection(n_set)) == len(m_set):
+                    cooked_data[n][n_key] = [x-y for x,y in 
+                    zip(cooked_data[n][n_key],cooked_data[m][m_key])]
+
+    # Set correction list length
+    correction = [0.0 for i in range(len(next(iter(cooked_data[n].values()))))]
+
+    # Add up all contributions to current correction
+    for key,val in cooked_data[n].items():
+        correction = [x+y for x,y in zip(correction,val)]
+
+    db[method][n][result]['cooked_data'] = cooked_data[n]
+    db[method][n][result]['correction']  = copy.deepcopy(correction)
+
+    # Final result value is the correction for n plus the result value from n-1
+    db[method][n][result][result] = copy.deepcopy(correction)
+    if n > 1: 
+        prev = db[method][n-1][result][result]
+        curr = db[method][n][result][result]
+        if prev != 0:
+            db[method][n][result][result] = [x+y for x,y in zip(curr,prev)]
+        else:
+            print("Previous result 0, {}-body jobs are likely not finished. Stopping harvest.".format(n-1))
+
+    # Wipe out data dicts for next result
+    cooked_data.clear()
+    raw_data.clear()
+
+
+def cutoff(db, method, n, full_result, dist = 0):
     '''
     Removes raw_data from the full_result based on the distance cutoff given and returns it
     NOTE: Need to employ a pass-able distance cutoff rather than the hardcoded database one
     '''
-    cut_result = collections.OrderedDict()
-    cut_result[n] = copy.deepcopy(db[method][n][full_result]['raw_data'])
-    for job in list(cut_result[n].keys()):
+    cut_result = copy.deepcopy(db[method][n][full_result]['raw_data']) # Copy full raw_data
+    for job in list(cut_result.keys()):
         job_set = set(job.split('_'))
-        if job_set <= set(db['close_solvent']):
+        if job_set <= set(db['close_solvent']): # Keep if all frags in close_solvent
             continue
         else:
-            del cut_result[n][job]
+            del cut_result[job] # Dump if any frags outside close_solvent
     return cut_result
 
 
 
-def extend(db, method, kwargs)
+def extend(db, method, result):
     '''
-    Extends the database to hold results from kwargs list
+    Extends the database to hold result
+    Also adds distance cutoff to db if not already present
     '''
-    farm = copy.deepcopy(db[method]['farm'])
-    for field in farm:
-        for result in kwarg:
+    if not 'distance' in db: # Add space for distance if needed
+        db['distance'] = False
+
+    if not result in db[method][1]: # Dict should be properly extended if 1 exists
+        farm = copy.deepcopy(db[method]['farm']) # All n-body
+        for field in farm:
             db[method][field][result] = collections.OrderedDict()
             db[method][field][result]['correction'] = 0
             db[method][field][result][result] = 0
-            db[method][field][result]['raw_data'] = 0
-            db[method][field][result]['cooked_data'] = 0
+            db[method][field][result]['raw_data'] = collections.OrderedDict()
+            db[method][field][result]['cooked_data'] = collections.OrderedDict()
+
+    return db
 
 
 def sanity_check(db):
